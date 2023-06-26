4ebcb43dcbe80e46bf6dcf0aadb5238ca11602f9
Lori Henderson <lorim95@gmail.com>
2023-06-26 14:44:56 -0400

    first commit, adds homepage navigation

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+        <div className="nav-items">
+            <span> <a href="/"> Home </a> </span>
+        </div>
+    </div>
+  )
+}
